860ccb892fc49203349f502a2c08af7110200a15
takeshi_honda <honda0924@hotmail.co.jp>
2022-02-17 02:10:22 +0900

    feat: update_done

--- src/queries/TaskQuery.ts
+++ src/queries/TaskQuery.ts
@@ -1,10 +1,21 @@
-import {useQuery} from "react-query";
+import {useQuery, useMutation, useQueryClient} from "react-query";
 import * as api from "../api/TaskAPI";
 
 const useTasks = () => {
   return useQuery('tasks', api.getTasks);
 }
 
+const useUpdateDoneTask = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(api.updateDoneTask, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('tasks')
+    }
+  })
+}
+
 export {
-  useTasks
+  useTasks,
+  useUpdateDoneTask
 }
