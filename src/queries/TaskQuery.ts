59bee7c151479165953895a6d7863253ac961353
takeshi_honda <honda0924@hotmail.co.jp>
2022-02-17 02:29:17 +0900

    feat: error handling

--- src/queries/TaskQuery.ts
+++ src/queries/TaskQuery.ts
@@ -1,5 +1,6 @@
 import {useQuery, useMutation, useQueryClient} from "react-query";
 import * as api from "../api/TaskAPI";
+import { toast } from "react-toastify";
 
 const useTasks = () => {
   return useQuery('tasks', api.getTasks);
@@ -10,7 +11,10 @@ const useUpdateDoneTask = () => {
 
   return useMutation(api.updateDoneTask, {
     onSuccess: () => {
-      queryClient.invalidateQueries('tasks')
+      queryClient.invalidateQueries('tasks');
+    },
+    onError: () => {
+      toast.error('更新に失敗しました。');
     }
   })
 }
